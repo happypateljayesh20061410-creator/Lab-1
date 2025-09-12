80a8f37afa6229b50fc8987d262bb71b56d6567a
happypateljayesh20061410-creator <happypateljayesh20061410@gmail.com>
2025-09-13 01:09:58 +0530

    Create substract.c

---
+++ substract.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ( ) {
+    int a,b,substract;
+
+    printf("Enter value of a : ");
+    scanf("%d",&a);
+    printf("Enter value of b : ");
+    scanf("%d",&b);
+
+    substract = a - b ;
+    printf("Substract is : %d",substract);
+
+}
