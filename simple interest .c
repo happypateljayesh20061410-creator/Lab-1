c0ca549c7e6413e53f1913242f9d61a2474c0966
Happy patel <happypateljayesh20061410@gmail.com>
2025-09-28 14:07:05 +0530

    Create  simple interest .c

---
+++ simple interest .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a,b,c,d;
+    printf("eneter your value A:");
+    scanf("%d",&a);
+    printf("enter your value B:");
+    scanf("%d",&b);
+     printf("enter your value C:");
+    scanf("%d",&c);
+    d=(a*b*c)/100;
+    printf("the simple intrest is =%d",d);
+    return 0;
+}
