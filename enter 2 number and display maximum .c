679b31e5e5f942585811c29bfb472aaf587ab7e8
Happy patel <happypateljayesh20061410@gmail.com>
2025-10-06 23:20:53 +0530

    Create enter 2 number and display maximum .c

---
+++ enter 2 number and display maximum .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter your two number:");
+    scanf("%d %d",&a,&b);
+if(a>b)
+{
+    printf(" maximum =%d",a);
+}
+else
+{
+    printf(" maximum=%d",b);
+}
+    return 0;
+}
